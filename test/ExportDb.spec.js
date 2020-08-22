baff2152f8792e110bb53d6ddbfa3cc909027ab9
George Moura <george@simples.vet>
2020-08-21 22:27:53 -0300

    Removendo arquivos .js

--- test/ExportDb.spec.js
+++
@@ -1,9 +0,0 @@
-expect = require('chai').expect
-
-ExportDb = require('../src/index')
-
-describe('ExportDb', () => {
-  it('shold show error when adapter not implemented', () => {
-    expect(() => new ExportDb('oracle')).to.throw(Error, 'Adapter oracle not found!');
-  })
-})
